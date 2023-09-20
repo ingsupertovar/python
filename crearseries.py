badc2e180ab6c42572dbd918629ee1ce3d81116e
ingsupertovar <40682819+ingsupertovar@users.noreply.github.com>
2023-09-20 17:57:27 -0500

    Update crearseries.py

--- crearseries.py
+++ crearseries.py
@@ -1,1 +1,10 @@
-print("Hola Mundo")
+# -*- coding: utf-8 -*-
+print "la suma de dos numeros"
+
+a = int(raw_input("numero"))
+
+b = int(raw_input("numero"))
+
+c = a + b
+
+print "la suma es %s" % c
